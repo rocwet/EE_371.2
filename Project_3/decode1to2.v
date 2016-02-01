fc43b08e04dec4e3903dc2ce43d2d171a625ada7
Minh Khuu <minhkhuu@uw.edu>
2016-01-31 16:26:19 -0800

    fix tab size

--- Project_3/decode1to2.v
+++ Project_3/decode1to2.v
@@ -14,18 +14,18 @@ input     |in         |1 bit    |Input of the decoder.
 input     |enable     |1 bit    |Enable decoder on active high.
 */
 module decode1to2(out, in, enable);
-	
-	/* define output and input ports */
+  
+  /* define output and input ports */
   output [1:0] out;
-	input in;
-	input enable;
-	
-	/* wires */
-	wire notin;
-	
-	/* gate logic */
-	not notinput (notin, in);
-	and output0  (out[0], notin, enable);
-	and output1  (out[1], in, enable);
-	
+  input in;
+  input enable;
+  
+  /* wires */
+  wire notin;
+  
+  /* gate logic */
+  not notinput (notin, in);
+  and output0  (out[0], notin, enable);
+  and output1  (out[1], in, enable);
+  
 endmodule
--- Project_3/decode3to8.v
+++ Project_3/decode3to8.v
@@ -14,20 +14,20 @@ input     |in         |3 bit    |Input of the decoder.
 input     |enable     |1 bit    |Enable decoder on active high.
 */
 module decode3to8(in, out, enable);
-	
-	/* define output and input ports */
+  
+  /* define output and input ports */
   output [7:0] out;
-	input  [2:0] in;
-	input enable;
-		
-	/* wires */
-	wire [1:0] dec1to2out;
+  input  [2:0] in;
+  input enable;
+    
+  /* wires */
+  wire [1:0] dec1to2out;
 
-	/* 1:2 decoders */
-	decode1to2 d12_0 (.in(in[2]), .out(dec1to2out), .enable(enable));
-	
-	/* 2:4 decoder */
-	decode2to4 d24_0 (.in(in[1:0]), .out(out[3:0]), .enable(dec1to2out[0]));
-	decode2to4 d24_1 (.in(in[1:0]), .out(out[7:4]), .enable(dec1to2out[1]));
+  /* 1:2 decoders */
+  decode1to2 d12_0 (.in(in[2]), .out(dec1to2out), .enable(enable));
+  
+  /* 2:4 decoder */
+  decode2to4 d24_0 (.in(in[1:0]), .out(out[3:0]), .enable(dec1to2out[0]));
+  decode2to4 d24_1 (.in(in[1:0]), .out(out[7:4]), .enable(dec1to2out[1]));
 
 endmodule
